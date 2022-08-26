2e120803a70f7b39f49dcf709fcc7d942621e4e9
Rishi Sharma <sharmarishi8997@gmail.com>
2022-08-27 00:50:21 +0530

    Time: 2 ms (69.76%), Space: 43.2 MB (90.68%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -0,0 +1,37 @@
+class Solution {
+    
+    ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+    public void createAdjacencyList(int [][] grid){
+        for(int r=0; r<grid.length; r++){
+            adj.add(new ArrayList<Integer>());
+            for(int c=0; c<grid[0].length; c++){
+                if(r==c) continue;
+                if(grid[r][c] == 1) adj.get(r).add(c);
+            }
+        }
+        
+    }
+    
+    public void dfs(int node, int[] vis, ArrayList<ArrayList<Integer>> adj){
+        vis[node] = 1;
+        for(int it : adj.get(node)){
+            if(vis[it] == 0){
+                dfs(it, vis, adj);
+            }
+        }
+    }
+    public int findCircleNum(int[][] isConnected) {
+        createAdjacencyList(isConnected);
+        int n = isConnected.length;
+        int[] vis = new int[n];
+        int count = 0;
+        for(int i=0; i<n; i++){
+            if(vis[i] == 0){
+                count++;
+                dfs(i, vis, adj);
+            }
+        }
+        return count;
+        
+    }
+}
